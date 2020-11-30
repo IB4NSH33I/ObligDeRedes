3106bdace5e1734b3f31e9e22d326b78bc73e416
Juan Martin Rodriguez <jumarod20@gmail.com>
2020-11-30 19:04:58 -0300

    Add Photo and User Class

--- Obligatorio Programacion de Redes/Domain/Comment.cs
+++ Obligatorio Programacion de Redes/Domain/Comment.cs
@@ -6,6 +6,8 @@ namespace Domain
 {
     class Comment
     {
+        public Photo Photo { get; set; }
+        public User Commenter { get; set; }
         public string Data { get; set; }
     }
 }
