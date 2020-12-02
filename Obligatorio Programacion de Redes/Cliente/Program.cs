c2470276cca8dcad725cef8ed4036bfb60d1373b
TOPOFGR <francogaleano8@gmail.com>
2020-12-01 22:33:20 -0300

    Added ClientSide register and login

--- Obligatorio Programacion de Redes/Cliente/Program.cs
+++ Obligatorio Programacion de Redes/Cliente/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Common.MessageProtocol;
+using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Cliente
 {
@@ -21,6 +23,52 @@ namespace Cliente
             socket.Connect(serverEndpoint);
 
             Console.WriteLine("Conectado al servidor!");
+
+            Console.WriteLine("Bienvenido al Sistema Client");
+            Console.WriteLine("Opciones validas: ");
+            Console.WriteLine("login -> Iniciar Sesion");
+            Console.WriteLine("register -> Registrar");
+            Console.WriteLine("exit -> abandonar el programa");
+            Console.WriteLine("Ingrese su opcion: ");
+
+            while (isRunning)
+            {
+                var opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "login":
+                        Console.WriteLine("Ingrese nombre de usuario:");
+                        var userLogin = Console.ReadLine();
+                        Console.WriteLine("Ingrese contraseña:");
+                        var passwordLogin = Console.ReadLine();
+                        Message loginMessage = new Message(HeaderConstants.Request, CommandConstants.Register, userLogin + "#" + passwordLogin);
+                        MessageProtocol.SendMessage(socket, loginMessage);
+                        break;
+
+                    case "exit":
+                        socket.Shutdown(SocketShutdown.Both);
+                        socket.Close();
+                        isRunning = false;
+                        break;
+
+                    case "register":
+                        Console.WriteLine("Ingrese nombre de usuario:");
+                        var user = Console.ReadLine();
+                        Console.WriteLine("Ingrese contraseña:");
+                        var password = Console.ReadLine();
+                        Message userMessage = new Message(HeaderConstants.Request, CommandConstants.Register, user + "#" +password);
+                        MessageProtocol.SendMessage(socket, userMessage);
+                        Message messageResponse = MessageProtocol.ReceiveMessage(socket);
+                        Console.WriteLine(messageResponse.MessageText);
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        break;
+                }
+            }
+            
+            Console.WriteLine("Cerrando Applicación");
 
         }
     }
